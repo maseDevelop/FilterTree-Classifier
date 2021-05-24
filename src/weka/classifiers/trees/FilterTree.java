d6c9e3b9401186b1cd1fd41435aa201db2ba51d8
Mason Elliott <mason.elliott12@gmail.com>
2021-05-24 18:06:13 +1200

    Before changes made

--- src/weka/classifiers/trees/FilterTree.java
+++ src/weka/classifiers/trees/FilterTree.java
@@ -7,17 +7,29 @@ import weka.filters.Filter;
 import weka.filters.unsupervised.instance.Randomize;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 public class FilterTree extends AbstractClassifier {
 
     /** for serialization */
     //static final long serialVersionUID = -251831442047263433L;
 
-    /** The kernel function to use. */
+    /** The filter to use. */
     protected Filter m_Filter = new Randomize();
 
     /** The minimum number of instances used to continue growing the tree */
     protected int m_minInstances = 100;
+
+    /** The root node of the tree**/
+    protected Node rootNode;
+
+    /** A list of instances that can be referenced for retrievial of an instance **/
+    protected Instances GlobalInstances;
+
+    /** Store computated sorted index of features **/
+    protected int sortedFeaturesIndex[][];
 
     /**
      * Returns the Capabilities of this classifier.
@@ -37,6 +49,7 @@ public class FilterTree extends AbstractClassifier {
         // class
         result.enable(Capabilities.Capability.NOMINAL_CLASS);
         result.enable(Capabilities.Capability.NUMERIC_CLASS);
+        result.enable(Capabilities.Capability.BINARY_CLASS);
         //result.enable(Capabilities.Capability.MISSING_CLASS_VALUES);
 
         return result;
@@ -65,12 +78,34 @@ public class FilterTree extends AbstractClassifier {
     /**
      * An interface indicating objects storing node information, implemented by three node info classes.
      */
-    protected interface NodeInfo extends Serializable {};
+    private interface NodeInfo extends Serializable {};
+
+    private class  RootNodeInfo implements NodeInfo{
+
+        // The data to be used for expanding the node.
+        protected Instances Data;
+
+
+        /**
+         * Constructs an Root Node object.
+         *
+         * @param data the data to be used for turning this node into an expanded node.
+         */
+        public RootNodeInfo(Instances data) {
+            Data = data;
+        }
+    }
 
     /**
      * Class whose objects represent split nodes.
      */
-    protected class SplitNodeInfo implements NodeInfo {
+    private class SplitNodeInfo implements NodeInfo {
+
+        // The left node
+        private Node left;
+
+        // The right node
+        private Node right;
 
         // The attribute used for splitting
         protected Attribute SplitAttribute;
@@ -78,27 +113,48 @@ public class FilterTree extends AbstractClassifier {
         // The split value
         protected double SplitValue;
 
-        // The array of successor nodes
-        protected Node[] Successors;
+        //Getter for left
+        protected Node getLeft() {return left;}
+
+        //Setter for left
+        protected void setLeft(Node node) {
+            this.left = node;
+            Instances[0] = node;
+        }
+
+        //Getter for right
+        protected Node getRight() {return right;}
+
+        //Setter for left
+        protected void setRight(Node node) {
+            this.right = node;
+            Instances[1] = node;
+        }
+
+        //Pair of instances
+        protected Node[] Instances;
 
         /**
          * Constructs a SplitNodeInfo object
          *
          * @param splitAttribute the attribute that defines the split
          * @param splitValue the value used for the split
-         * @param successors the array of successor nodes
+         * @param left access to the left side of the tree
+         * @param right access to the right side of the tree
          */
-        public SplitNodeInfo(Attribute splitAttribute, double splitValue, Node[] successors) {
+        public SplitNodeInfo(Attribute splitAttribute, double splitValue, Node left, Node right) {
             SplitAttribute = splitAttribute;
             SplitValue = splitValue;
-            Successors = successors;
+            this.left = left;
+            this.right = right;
+            Instances = new Node[]{left,right};
         }
     }
 
     /**
      * Class whose objects represent leaf nodes.
      */
-    protected class LeafNodeInfo implements NodeInfo {
+    private class LeafNodeInfo implements NodeInfo {
 
         // The array of predictions
         protected double[] Prediction;
@@ -116,7 +172,7 @@ public class FilterTree extends AbstractClassifier {
     /**
      * Class whose objects represent unexpanded nodes.
      */
-    protected class UnexpandedNodeInfo implements NodeInfo {
+    private class UnexpandedNodeInfo implements NodeInfo {
 
         // The data to be used for expanding the node.
         protected Instances Data;
@@ -134,7 +190,7 @@ public class FilterTree extends AbstractClassifier {
     /**
      * Class representing a node in the decision tree.
      */
-    protected class Node implements Serializable {
+    private class Node implements Serializable {
 
         // The node information object that stores the actual information for this node.
         protected NodeInfo NodeInfo;
@@ -149,6 +205,16 @@ public class FilterTree extends AbstractClassifier {
         }
     }
 
+
+    protected double calculateEntropy(Node node){
+        return 0;
+    }
+
+    protected Node makeLeafNode(Node node){
+
+        return null;
+    }
+
     /**
      * Method that processes a node. Assumes that the given node is unexpanded. Turns the node
      * into a leaf node or split node as appropriate by replacing the node information.
@@ -157,7 +223,21 @@ public class FilterTree extends AbstractClassifier {
      * @return the node with updated node information, turning it into a split node or leaf node
      */
     protected Node processNode(Node node) {
+
+        Instances instances = ((UnexpandedNodeInfo)node.NodeInfo).Data;
+
+        //Checking stopping criteria - Certain Number of instances met, as specified by the user
+        if(instances.size() <= m_minInstances){
+            return makeLeafNode(node);
+        }
+
+        //Iterating through each instance and calculating the entropy
+
+
+
+
         return null;
+
     }
 
 
@@ -169,6 +249,28 @@ public class FilterTree extends AbstractClassifier {
     @Override
     public void buildClassifier(Instances instances) throws Exception {
 
+        //Setting the list of global instances
+        GlobalInstances = instances;
+
+
+        //Creating a node list that has all tree nodes in it before they are finalised.
+        ArrayList<Node> nodesInTree = new ArrayList<>();
+
+        //Creating rootNode
+        rootNode = new Node(new UnexpandedNodeInfo(instances));
+
+        //Adding node to tree
+        nodesInTree.add(rootNode);
+
+        //Splitting node
+        while(!nodesInTree.isEmpty()){
+            Node newNode = processNode(rootNode);
+
+            //Iterating through node subtrees and expanding to ethier split node or leafnode
+            if(newNode.NodeInfo instanceof SplitNodeInfo){
+                nodesInTree.addAll(Arrays.asList(((SplitNodeInfo) newNode.NodeInfo).Instances));
+            }
+        }
     }
 
     @Override
